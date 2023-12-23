2dd23ac02140e7ef42da2b118e8abec70b35d713
BalaGanesh30 <balaganesh9596@gmail.com>
2023-12-23 19:50:25 +0530

    change in element id login

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.getElementById('loginview').addEventListener('submit', function(event) {
+document.getElementById('login').addEventListener('submitlogin', function(event) {
     event.preventDefault(); // Prevent the form from submitting
   
     // Get input values
@@ -10,7 +10,7 @@ document.getElementById('loginview').addEventListener('submit', function(event) 
     // Simulated user data (replace this with your actual user data)
     const users = [
       { username: 'user1', password: 'password1', gender: 'male' },
-      { username: 'user2', password: 'password2', gender : 'female' }
+      { username: 'user2', password: 'balaganesh2', gender : 'female' }
     ];
   
     // Check if the entered username and password match any user in the 'users' array
@@ -18,11 +18,11 @@ document.getElementById('loginview').addEventListener('submit', function(event) 
   
     if (user) {
       // Authentication successful
-      document.getElementById('loginview').innerText = 'Login successful';
+      document.getElementById('login').innerText = 'Login successful';
       // You might redirect to another page or perform other actions here after successful login
     } else {
       // Authentication failed
-      document.getElementById('loginview').innerText = 'Invalid username or password';
+      document.getElementById('login').innerText = 'Invalid username or password';
     }
   });
 
